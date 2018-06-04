da0212390f3bf3ceaa36ec03fb1059fb36a6b80c
Brad R. Marshall <bmarshall@sped.mobi>
2018-06-04 18:10:39 -0500

    Added Windows Customizations script

---
+++ Windows Customizations.ps1
@@ -0,0 +1,36 @@
+﻿<#
+
+    Windows Customization Script
+
+#>
+
+Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name 'ShowSecondsInSystemClock' -Value 1 -Force -Verbose
+
+
+Set-ItemProperty "HKLM:\SOFTWARE\Policies\Microsoft\Windows\System" -Name 'DisableLogonBackgroundImage' -Value 1 -Force -Verbose
+
+
+
+Set-ItemProperty "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name 'AppsUseLightTheme' -Value 0 -Force -Verbose
+
+if (!(Test-Path "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\MTCUVC"))
+{
+    New-Item "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\MTCUVC" -Force -Verbose
+
+}
+
+
+Set-ItemProperty "HKLM:\Software\Microsoft\Windows NT\CurrentVersion\MTCUVC\" -Name 'EnableMtcUvc' -Value 0 -Force -Verbose
+
+
+Set-ItemProperty "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Authentication\LogonUI\TestHooks" -Name 'Threshold' -Value 1 -Force -Verbose
+
+
+# Set-ItemProperty "HKLM:\Software\Microsoft\WindowsUpdate\UX" -Name 'IsConvergedUpdateStackEnabled' -Value 0 -Force -Verbose
+
+
+
+
+
+
+
